8466e45f21dd5975978231a4bad270a194793af5
Mostofa2 <mostofaahmed550@gmail.com>
2021-09-20 00:24:12 +0600

    ajker moto akhanei ses

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// double a number
+
 function doubleIt(num) {
     let double = num + num;
     return double;
@@ -6,3 +8,27 @@ function doubleIt(num) {
 
 let result = doubleIt(6);
 console.log(result);
+
+// fibonacci number 
+
+function fibonacciIt(num) {
+    let fibonacci = 1;
+    for (let i = 1; i <= num; i++) {
+        fibonacci = fibonacci * i;
+    }
+    return fibonacci;
+}
+
+console.log(fibonacciIt(1));
+
+// sumatoion of a number of series
+
+function sumItAll(num) {
+    let sum = 0;
+    for (i = 0; i <= num; i++) {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+console.log(sumItAll(10));
